0b4700cb2a43f2b3bada304ed05b5ee98b765306
Matthew Rayermann <matthew.rayermann@gmail.com>
2014-01-08 00:12:38 -0600

    Basic game client created.

---
+++ src/com/rolledback/gui/Launcher.java
@@ -0,0 +1,68 @@
+package com.rolledback.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+
+import com.rolledback.framework.Game;
+
+public class Launcher {
+   
+   static Game newGame;
+   
+   public static void main(String args[]) {      
+      //init(104, 48); //1920x1080 @ 16x16 tiles
+      //init(52, 24); //1920x1080 @ 32x32 tiles
+      init(26, 12); //1920x1080 @ 64x64 tiles
+      //init(13, 6);  //1920x1080 @ 128x128 tiles
+   }
+   
+   public static void init(int x, int y) {
+      JFrame frame = new JFrame("TBS2");
+      
+      int screenHeight = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
+      int screenWidth = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+      
+      screenHeight -= (int)((double)screenHeight / 10);
+      screenWidth -= (int)((double)screenWidth / 10);
+
+      while(screenWidth % 64 != 0 || screenWidth % 32 != 0 || screenWidth % 128 != 0 || screenWidth % 16 != 0)
+         screenWidth--;
+      while(screenHeight % 64 != 0 || screenHeight % 32 != 0 || screenHeight % 128 != 0 || screenHeight % 16 != 0)
+         screenHeight--;
+      
+      int gameWidth = x;
+      int gameHeight = y;
+      int guiHeight = 128;
+      
+      int tileSize = 128;
+      while((gameWidth * tileSize > screenWidth || gameHeight * tileSize > screenHeight - guiHeight) && tileSize >= 16) {
+         tileSize /= 2;
+      }
+      
+      if(tileSize < 16) {
+         System.out.println("Bad dimensions.");
+         System.out.println("Final width attempted: " + (gameWidth * 16) + " w/screen width: " + screenWidth);
+         if(gameWidth * 16 > screenWidth)
+            System.out.println("Make game less wide.");
+         System.out.println("Final height attempted: " + (gameHeight * 16) + " w/screen height: " + screenHeight);
+         if(gameHeight * 16 > screenHeight)
+            System.out.println("Make game less tall.");
+         System.exit(-1);
+      }
+      System.out.println(screenWidth);
+      int offsetHorizontal = screenWidth - (gameWidth * tileSize);
+      int offsetVertical = screenHeight - guiHeight - (gameHeight * tileSize);
+      newGame = new Game(x, y, tileSize, offsetHorizontal / 2, offsetVertical / 2, guiHeight);
+      newGame.setDoubleBuffered(true);
+      newGame.setSize(screenWidth, screenHeight);
+      frame.getContentPane().add(newGame);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setResizable(false);
+      frame.setVisible(true);
+      frame.setSize(screenWidth + frame.getInsets().right + frame.getInsets().left, screenHeight + frame.getInsets().top + frame.getInsets().bottom);
+      //newGame.switchTeams();
+   }
+   
+}
---
+++ src/com/rolledback/terrain/River.java
@@ -0,0 +1,15 @@
+package com.rolledback.terrain;
+
+import com.rolledback.framework.World;
+import com.rolledback.terrain.Tile.TILE_TYPE;
+
+public class River extends Tile {
+   
+   public River(World w, int x, int y) {
+      super(w, x, y, new TerrainEffect(0, 0, 0), 'm');
+      type = TILE_TYPE.RIVER;
+      vehiclePassable = false;
+      infantryPassable = false;
+   }
+   
+}
